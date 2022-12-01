c8cd4fcb6906ecb904b98ecaa5a32585b6cf069b
aqaqsubin <aqaq925120k@cnu.ac.kr>
2022-12-01 15:38:28 +0900

    :tada: Django Project 시작

---
+++ order/views.py
@@ -0,0 +1,12 @@
+import os, json
+from .models import *
+from os.path import join as pjoin
+
+from django.conf import settings
+from django.utils import timezone
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, redirect
+
+
+def home(request):
+    return render(request, 'order/home.html')

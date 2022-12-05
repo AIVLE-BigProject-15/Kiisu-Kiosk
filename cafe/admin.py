209bbed57fc106d7264a74bac440c1e66ec6b5ad
dear_ddyd <68991416+dearddyd@users.noreply.github.com>
2022-12-05 10:22:19 +0900

    admin 추가, admin페이지 커스텀
    ID: admin, PW: admin
    admin 페이지 커스텀

--- cafe/admin.py
+++ cafe/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 from django.conf import settings
 from .models import *
 
-admin.site.register(Menu)
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Model)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=['type', 'title', 'price']
+    
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['age_group', 'version', 'created']
+    
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id', 'menu', 'created']
+    
+class ModelAdmin(admin.ModelAdmin):
+    list_display=['version', 'type', 'is_active']
+    actions=['activate', 'deactivate']
+
+    def activate(self, request, queryset):
+        queryset.update(is_active= True ) #queryset.update
+    activate.short_description = '지정 모델을 active 상태로 변경'
+    
+    def deactivate(self, request, queryset):
+        queryset.update(is_active= False) #queryset.update
+    deactivate.short_description = '지정 모델을 deactive 상태로 변경'
+        
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Model, ModelAdmin)

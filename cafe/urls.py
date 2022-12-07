c03c8282d9197d2a82b944fb50266c20694551a0
조진호 <68462676+JoJinho333@users.noreply.github.com>
2022-12-07 11:17:37 +0900

    web_temp_jh

--- cafe/urls.py
+++ cafe/urls.py
@@ -6,8 +6,8 @@ app_name = 'cafe'
 urlpatterns = [
     path('', views.home, name='home'),
     path('order', views.order, name='order'),
-    path('confirm', views.confirm, name='confirm'),
-    path('pay', views.pay, name='pay'),
+    # path('camera', views.camera, name='camera'),
+    path('detect_age_group', views.detect_age_group, name='detect_age_group'),
 ]
 
 

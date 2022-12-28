538c6feed90639d7d3f1f32bc8f42334c93200cc
aqaqsubin <aqaq925120k@cnu.ac.kr>
2022-12-28 16:51:29 +0900

    :lipstick: 메뉴 타이틀 필터 추가

--- cafe/templatetags/post_extras.py
+++ cafe/templatetags/post_extras.py
@@ -7,3 +7,11 @@ register = template.Library()
 @register.filter
 def fix_route(value):
     return "/media" + os.sep + f"{os.sep}".join(value.path.split(os.sep)[-3:])
+
+
+@register.filter
+def fix_title(title):
+    title = title.strip()
+    if len(title) <= 7: return title
+    print('\n'.join(title.split()))
+    return '\n'.join(title.split())
